d4d4f2127a54d1a9ad717151ba0e6d1135f9b8a9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-01 11:27:52 +0100

    Heading and working on user model

--- Backend/config/dbConnect.js
+++ Backend/config/dbConnect.js
@@ -1,2 +1,11 @@
 const mongoose = require("mongoose");
-const dbConnect = async;
+
+const dbConnect = async () => {
+  try {
+    const connect = mongoose.connect("mongodb://localhost:27017+");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = dbConnect;
