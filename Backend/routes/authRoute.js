6a177718d57c8b5d057a5375f863a6634b8558e6
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-01 12:27:57 +0100

    Working on userRoutes

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const { createUser } = require("../controllers/userCtrl");
 const router = express.Router();
 
-router.post("/register");
+router.post("/register", createUser);
 
 module.exports = router;
