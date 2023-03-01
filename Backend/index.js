b2b74c687fde0088af6649b4972fd942905e8184
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-01 16:17:26 +0100

    All bugs fixed

--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const authRoute = require("./routes/authRoute");
 const dbConnect = require("./config/dbConnect");
+const bodyParser = require("body-parser");
+
+const app = express();
 
 // DB connection...
 dbConnect();
-
-const app = express();
+// Middleware...
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+
+// Routes
 app.use("/api/v1/users", authRoute);
 
 const PORT = process.env.PORT || 3000;
