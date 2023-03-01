6c35e94436497faeb4fea76153ba90f7f7041f03
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-01 16:22:35 +0100

    creating errorHandler middleware

--- Backend/middlewares/errorHandler.js
+++ Backend/middlewares/errorHandler.js
@@ -1,4 +1,6 @@
 // Not Found Error
 const notFound = (req, res, next) => {
-  const error = new Error(`Not found ${req?.originalUrl}`)
-}
+  const error = new Error(`Not found ${req?.originalUrl}`);
+  res.status(404);
+  next(error);
+};
