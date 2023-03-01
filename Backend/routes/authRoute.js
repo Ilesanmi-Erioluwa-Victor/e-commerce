512fadac6bbe8ad91d831025b63fa9c7a2d5c83f
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-01 23:55:28 +0100

    Worked on deleteUserCtrl route and controller

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -12,6 +12,6 @@ router.post("/register", createUserCtrl);
 router.post("/login", loginUserCtrl);
 router.get("/:id", getUserCtrl);
 router.get("/", getAllUsersCtrl);
-router.get("/:id", deleteUserCtrl);
+router.delete("/:id", deleteUserCtrl);
 
 module.exports = router;
