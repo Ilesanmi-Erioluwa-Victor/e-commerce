25f5d0b470b3f6bcddd36253620e1fb2bbcd69f2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-01 12:16:49 +0100

    Working on userRoutes

--- Backend/controllers/userCtrl.js
+++ Backend/controllers/userCtrl.js
@@ -1,5 +1,15 @@
-const userModel = require("../models/userModel");
+const User = require("../models/userModel");
 
 exports.createUser = async (req, res) => {
-
-}
+    const email = req?.body?.email;
+    
+    const findUser = await User.findOne(email);
+    if (!findUser) {
+        // Create a new user
+        const user = await User.create({
+            fir
+        })
+    } else {
+        
+    }
+};
