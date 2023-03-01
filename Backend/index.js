ea4ef5f0f6e05b13f5c326f0273b589b3160ea9d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-01 11:58:52 +0100

    Working on userRoutes

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
+const authRoute = require("./routes/authRoute");
 
 const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1/auths", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}..`);
