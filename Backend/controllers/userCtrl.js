e968db7b13bcc7b3b8ff6c9f90de8b349f2747d5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-01 12:21:31 +0100

    Working on userRoutes

--- Backend/controllers/userCtrl.js
+++ Backend/controllers/userCtrl.js
@@ -1,18 +1,22 @@
 const User = require("../models/userModel");
+const httpStatus = require("http-status");
 
 exports.createUser = async (req, res) => {
-    const email = req?.body?.email;
-    
-    const findUser = await User.findOne(email);
-    if (!findUser) {
-        // Create a new user
-        const user = await User.create({
-            firstName: req?.body?.firstName,
-            lastName: req?.body?.lastName,
-            email: req?.body?.email,
-            password : req?.body?.password
-        })
-    } else {
-        
-    }
+  const email = req?.body?.email;
+
+  const findUser = await User.findOne(email);
+  if (!findUser) {
+    // Create a new user
+    const user = await User.create({
+      firstName: req?.body?.firstName,
+      lastName: req?.body?.lastName,
+      email: req?.body?.email,
+      password: req?.body?.password,
+    });
+    res.status(httpStatus.CREATED).json({
+      status: "success",
+      user,
+    });
+  } else {
+  }
 };
