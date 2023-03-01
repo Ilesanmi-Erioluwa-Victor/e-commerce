3dd0bc648a23db423b3aacfdc65cdd4a9cb08aba
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-01 12:50:26 +0100

    Working on userRoutes

--- Backend/config/dbConnect.js
+++ Backend/config/dbConnect.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 const dbConnect = async () => {
-  try {
-    const connect = mongoose.connect("mongodb://localhost:27017+");
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+      mongoose.set("strictQuery", false);
+        await mongoose.connect(process.env.MONGODB_URL, {});
+        console.log("DB Successfully connected..")
+    } catch (error) {
+      console.log(error.message);
+    }
 };
 
 module.exports = dbConnect;
--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const authRoute = require("./routes/authRoute");
+const { db } = require("./models/userModel");
 
+db()
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/v1/auths", authRoute);
+app.use("/api/v1/users", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}..`);
