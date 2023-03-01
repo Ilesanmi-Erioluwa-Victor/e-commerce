440861584654258cee8cfcac9be3085ae877fa3a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-01 12:07:02 +0100

    Working on userRoutes

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -28,4 +28,5 @@ const userSchema = new mongoose.Schema({
 });
 
 //Export the model
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
+module.exports = User;
