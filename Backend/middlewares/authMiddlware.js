8e08b3a8e58dc98ccea0b4685566a7af2e7b38af
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-04 18:06:00 +0100

    working on authmiddleware

--- Backend/middlewares/authMiddlware.js
+++ Backend/middlewares/authMiddlware.js
@@ -1,3 +1,8 @@
 const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
 
+const authMiddleware = asyncHandler(async (req, res, next) => {
+    
+})
 
