788b021257ccabde8a234312c67f09997448eaa2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-04 18:10:16 +0100

    working on authmiddleware

--- Backend/middlewares/authMiddlware.js
+++ Backend/middlewares/authMiddlware.js
@@ -5,9 +5,14 @@ const jwt = require("jsonwebtoken");
 const authMiddleware = asyncHandler(async (req, res, next) => {
     let token;
     if (req?.header?.authorization?.startsWith("Bearer")) {
-        
+        token = req?.headers?.authorization?.split(" ")[1];
+        try {
+            
+        } catch (error) {
+            
+        }
     } else {
-        throw new Error("")
+        throw new Error("There is no token attached to your header...")
     }
 })
 
