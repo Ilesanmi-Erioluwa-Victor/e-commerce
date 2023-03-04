ece263cadd78cb52c34bae269fb1b09e5ec97877
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-04 18:07:55 +0100

    working on authmiddleware

--- Backend/middlewares/authMiddlware.js
+++ Backend/middlewares/authMiddlware.js
@@ -3,6 +3,11 @@ const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = asyncHandler(async (req, res, next) => {
-    
+    let token;
+    if (req?.header?.authorization?.startsWith("Bearer")) {
+        
+    } else {
+        throw new Error("")
+    }
 })
 
