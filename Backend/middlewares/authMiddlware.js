f8f19643ff11c5797c57287e1fa2e978297f7dd7
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-04 18:15:33 +0100

    working on authmiddleware

--- Backend/middlewares/authMiddlware.js
+++ Backend/middlewares/authMiddlware.js
@@ -3,18 +3,20 @@ const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = asyncHandler(async (req, res, next) => {
-    let token;
-    if (req?.header?.authorization?.startsWith("Bearer")) {
-        token = req?.headers?.authorization?.split(" ")[1];
-        try {
-            if (token) {
-                const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-            }
-        } catch (error) {
-           throw new Error("Not authorized, token expired, Please login !!! ") 
-        }
-    } else {
-        throw new Error("There is no token attached to your header...")
+  let token;
+  if (req?.header?.authorization?.startsWith("Bearer")) {
+    token = req?.headers?.authorization?.split(" ")[1];
+    try {
+      if (token) {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        console.log(decoded);
+      }
+    } catch (error) {
+      throw new Error("Not authorized, token expired, Please login !!! ");
     }
-})
+  } else {
+    throw new Error("There is no token attached to your header...");
+  }
+});
 
+module.exports = authMiddleware;
