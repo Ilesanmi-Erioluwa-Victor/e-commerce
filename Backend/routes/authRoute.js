cc25de9679284cc63311077c70a52490b0f7fb45
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-04 18:18:18 +0100

    working on authmiddleware

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -7,11 +7,14 @@ const {
   deleteUserCtrl,
   updateUserCtrl
 } = require("../controllers/userCtrl");
+const authMiddleware = require("../middlewares/authMiddlware");
+
+
 const router = express.Router();
 
 router.post("/register", createUserCtrl);
 router.post("/login", loginUserCtrl);
-router.get("/:id", getUserCtrl);
+router.get("/:id",authMiddleware, getUserCtrl);
 router.get("/", getAllUsersCtrl);
 router.delete("/:id", deleteUserCtrl);
 router.put("/:id", updateUserCtrl);
