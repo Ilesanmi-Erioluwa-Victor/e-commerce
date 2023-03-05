628f31e645684c2e067525818b88757bf7726954
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-05 09:50:52 +0100

    Working on createProductCtrl

--- Backend/controllers/productCtrl.js
+++ Backend/controllers/productCtrl.js
@@ -2,15 +2,25 @@ const Product = require("../models/productModel");
 const httpStatus = require("http-status");
 const asyncHandler = require("express-async-handler");
 
+// Create Product
 exports.createProductCtrl = asyncHandler(async (req, res) => {
   try {
     const product = await Product.create(req?.body);
-    
-      res.json(product);
+
+    res.json(product);
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
+});
 
+// Get product
+exports.getProductCtrl = asyncHandler(async (req, res) => {
+  const { id } = req?.params;
+  try {
+    const product = await Product.findById(id);
 
-
+    res.json(product);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
--- Backend/models/productModel.js
+++ Backend/models/productModel.js
@@ -31,6 +31,7 @@ const productSchema = new mongoose.Schema(
     },
     quantity: {
       type: Number,
+      required : true
     },
 
     images: {
