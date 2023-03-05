96516c445b2845811ed492682e3a8af78dd77f3e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-05 01:54:55 +0100

    Working on Products ctrl and routes

--- Backend/controllers/productCtrl.js
+++ Backend/controllers/productCtrl.js
@@ -3,5 +3,7 @@ const httpStatus = require("http-status");
 const asyncHandler = require("express-async-handler");
 
 exports.createProductCtrl = asyncHandler(async (req, res) => {
-    const product =  await Product.create()
+  const product = await Product.create();
+
+  res.send("Hello from products...");
 });
