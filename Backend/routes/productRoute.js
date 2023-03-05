c79575ff0b6aeac406c4624dda901882aa56fc9b
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-05 09:53:24 +0100

    Create getProduct route and controler

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const { createProductCtrl } = require("../controllers/productCtrl");
+const {
+  createProductCtrl,
+  getProductCtrl,
+} = require("../controllers/productCtrl");
 
 const router = express.Router();
 
 router.post("/", createProductCtrl);
+
+router.route("/:id").get(getProductCtrl);
 module.exports = router;
