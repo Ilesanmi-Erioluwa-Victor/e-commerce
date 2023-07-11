1c0499ec6e36428daf9d1a734215f166fec71d7a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-11 22:06:25 +0100

    Working on products model

--- Backend/controllers/productCtrl.js
+++ Backend/controllers/productCtrl.js
@@ -77,9 +77,9 @@ exports.getAllProductsCtrl = asyncHandler(async (req, res) => {
     // const products = await Product.where("category").equals(req.query.category)
     const queryObj = { ...req.query };
     const excludeFields = ["page", "sort", "limits", "fields"];
-
-    excludeFields.forEach((el) => delete queryObj[el]);
-    console.log(queryObj);
+     const queryObj2 =  excludeFields.forEach((el) => delete queryObj[el]);
+    
+    console.log(queryObj, queryObj2);
     const products = await Product.find({
       brand: req.query.brand,
       category: req.query.category,
