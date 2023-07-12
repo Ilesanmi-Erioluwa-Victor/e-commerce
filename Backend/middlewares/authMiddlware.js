39a279b4953e8ad48df828d687f5b450257e5675
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-12 10:25:21 +0100

    Facing bug in createProductctrl

--- Backend/middlewares/authMiddlware.js
+++ Backend/middlewares/authMiddlware.js
@@ -14,7 +14,6 @@ const authMiddleware = asyncHandler(async (req, res, next) => {
          const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
          const user = await User.findById(decoded?.id);
          req.user = user;
-
          next();
       }
     } catch (error) {
