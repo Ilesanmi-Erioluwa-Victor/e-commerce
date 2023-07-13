25c619bbe858777da386f96c78b2e204426698d2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-13 18:37:43 +0100

    working on sending email with nodemailer

--- Backend/controllers/emailCtrl.js
+++ Backend/controllers/emailCtrl.js
@@ -2,5 +2,33 @@ const nodemailer = require("nodemailer")
 const asyncHandler = require('express-async-handler');
 
 exports.sendMail = asyncHandler(async (data, res, req) => {
-    
+   var transport = nodemailer.createTransport({
+     host: 'smtp.gmail.com',
+     port: 2525,
+     auth: {
+       user: `${process.env.NODEMAILER_USERNAME}`,
+       pass: `${process.env.NODEMAILER_PASS}`,
+     },
+   });
+
+   const resetUrl = `If you were requested to reset your account password, reset now, otherwise ignore this message
+        <a href= ${req.protocol}://${req.get(
+     'host'
+   )}/api/v1/users/verify_account/${verificationToken}>Click to verify..</a>
+       `;
+
+   const mailOptions = {
+     from: "Ifelola <'ifedayo1452@gmail.com>'",
+     to: 'ericjay1452@gmail.com',
+     subject: 'Account Verification ',
+     text: 'Hey there, it’s our first message sent with Nodemailer 😉 ',
+     html: resetUrl,
+   };
+
+   transport.sendMail(mailOptions, (error, info) => {
+     if (error) {
+       return console.log(error);
+     }
+     res.json(resetUrl);
+   });  
 })
