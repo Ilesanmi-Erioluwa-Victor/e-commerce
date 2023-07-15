1ec2311864715ddeeb26747199438c3ea16f3193
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-15 19:46:59 +0100

    Working on resetPassword ctrl

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const {
   createUserCtrl,
   loginUserCtrl,
@@ -11,24 +11,26 @@ const {
   RefreshTokenHandlerCtrl,
   logOutCtrl,
   updatePassword,
-  forgotPasswordToken
-} = require("../controllers/userCtrl");
-const { authMiddleware, isAdmin } = require("../middlewares/authMiddlware");
+  forgotPasswordToken,
+  resetPassword,
+} = require('../controllers/userCtrl');
+const { authMiddleware, isAdmin } = require('../middlewares/authMiddlware');
 
 const router = express.Router();
 
-router.post("/register", createUserCtrl);
-router.post("/login", loginUserCtrl);
-router.get("/refresh", RefreshTokenHandlerCtrl);
-router.post("/forgot-password", forgotPasswordToken)
-router.get("/logout", logOutCtrl);
-router.put("/password",authMiddleware, updatePassword)
-router.get("/:id", authMiddleware, isAdmin, getUserCtrl);
-router.get("/", getAllUsersCtrl);
+router.post('/register', createUserCtrl);
+router.post('/login', loginUserCtrl);
+router.get('/refresh', RefreshTokenHandlerCtrl);
+router.post('/forgot-password', forgotPasswordToken);
+router.post('/reset-password/:token', resetPassword);
+router.get('/logout', logOutCtrl);
+router.put('/password', authMiddleware, updatePassword);
+router.get('/:id', authMiddleware, isAdmin, getUserCtrl);
+router.get('/', getAllUsersCtrl);
 
-router.delete("/:id",authMiddleware, deleteUserCtrl);
-router.put("/edit-user", authMiddleware, updateUserCtrl);
-router.put("/block-user/:id", authMiddleware, isAdmin, blockUserCtrl);
-router.put("/unblock-user/:id", authMiddleware, isAdmin, unBlockUserCtrl);
+router.delete('/:id', authMiddleware, deleteUserCtrl);
+router.put('/edit-user', authMiddleware, updateUserCtrl);
+router.put('/block-user/:id', authMiddleware, isAdmin, blockUserCtrl);
+router.put('/unblock-user/:id', authMiddleware, isAdmin, unBlockUserCtrl);
 
 module.exports = router;
