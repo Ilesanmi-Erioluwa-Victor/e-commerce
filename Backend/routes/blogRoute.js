b1ffd1f5b5745505b59b5500c7abfe276eb38494
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-25 00:49:27 +0100

    Working on createBlog

--- Backend/routes/blogRoute.js
+++ Backend/routes/blogRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
+const { authMiddleware, isAdmin } = require('../middlewares/authMiddlware');
 
 const blogRoute = express.Router();
-
+blogRoute.post("/", authMiddleware, isAdmin)
 module.exports = blogRoute;
