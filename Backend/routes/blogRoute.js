2c2e100ef24173170726e105d09d5768cb3a6d87
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-25 06:34:50 +0100

    Working on updateblog

--- Backend/routes/blogRoute.js
+++ Backend/routes/blogRoute.js
@@ -5,4 +5,5 @@ const { createBlog, updateBlog } = require('../controllers/blogCtrl');
 const blogRoute = express.Router();
 blogRoute.post('/', authMiddleware, isAdmin, createBlog);
 blogRoute.put('/:id', authMiddleware, isAdmin, updateBlog);
+blogRoute.put('/:id', authMiddleware, isAdmin, updateBlog);
 module.exports = blogRoute;
