bccaf92f05cd201b7596706a6db4ea9e83888ce0
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-25 07:04:30 +0100

    Working on deleteblog

--- Backend/routes/blogRoute.js
+++ Backend/routes/blogRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddlware');
-const { createBlog, updateBlog, getBlog, getBlogs } = require('../controllers/blogCtrl');
+const { createBlog, updateBlog, getBlog, getBlogs, deleteBlog } = require('../controllers/blogCtrl');
 
 const blogRoute = express.Router();
 blogRoute.post('/', authMiddleware, isAdmin, createBlog);
 blogRoute.put('/:id', authMiddleware, isAdmin, updateBlog);
 blogRoute.get('/:id', getBlog);
 blogRoute.get('/', authMiddleware, isAdmin, getBlogs);
+blogRoute.delete('/', authMiddleware, isAdmin, deleteBlog);
 module.exports = blogRoute;
