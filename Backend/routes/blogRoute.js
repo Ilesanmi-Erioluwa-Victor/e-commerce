1b69624f649f7a558563f41337673924007745fc
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-25 06:58:30 +0100

    Working on getblog

--- Backend/routes/blogRoute.js
+++ Backend/routes/blogRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddlware');
-const { createBlog, updateBlog, getBlog } = require('../controllers/blogCtrl');
+const { createBlog, updateBlog, getBlog, getBlogs } = require('../controllers/blogCtrl');
 
 const blogRoute = express.Router();
 blogRoute.post('/', authMiddleware, isAdmin, createBlog);
 blogRoute.put('/:id', authMiddleware, isAdmin, updateBlog);
 blogRoute.get('/:id', getBlog);
+blogRoute.get('/', authMiddleware, isAdmin, getBlogs);
 module.exports = blogRoute;
